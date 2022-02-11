ad63b743a405d86fe0768769c4bfc5ab914004c8
Derek Emsbach <demsbach92@gmail.com>
2022-02-10 21:47:50 -0700

    4 problems

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -10,7 +10,13 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// your code here!
+const arrowRestSum = (num, ...all) => {
+  let sum = num
+  all.forEach(n => {
+    sum += n
+  })
+  return sum
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
